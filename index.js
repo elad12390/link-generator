55ccc9d3c9a6f442e7cb14db550b96e114afa55d
Elad <elad12390@gmail.com>
2021-11-04 20:18:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const baseUrl = `https://blalbla.blala.com/`;
+app.get('/:idTour/:participants/', (req, res) => {
+  const { idTour, participants } = req.params;
+  const result = Array.from({length: participants}, (_, i) => `${baseUrl}?tourId=${idTour}&userCode=${i + 1}`);
+  res.send(`<pre>${result.join('\n')}</pre>`);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
