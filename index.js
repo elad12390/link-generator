7352c3c192445b1cd7a811b224c5919891868e96
Elad <elad12390@gmail.com>
2021-11-04 20:24:38 +0200

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const baseUrl = `https://blalbla.blala.com/`;
 app.get('/:idTour/:participants/', (req, res) => {
   const { idTour, participants } = req.params;
